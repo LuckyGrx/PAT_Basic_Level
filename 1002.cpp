fff2c7fa9464064814aae005d26fb1b8d4020d49
LuckyGan <grx15002364225@hotmail.com>
2017-04-03 19:02:41 +0800

    pat

---
+++ 1002.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<string>
+#include<sstream>
+using namespace std;
+string hashArray[10] = { "ling","yi","er","san","si","wu","liu","qi","ba","jiu" };
+
+int main()
+{
+	string str;
+	cin >> str;
+	int sum = 0;
+	for (int i = 0; i < str.size(); i++)
+		sum += str[i] - '0';
+
+	stringstream ss;
+	string sumString;
+	ss << sum;
+	ss >> sumString;
+	ss.clear();
+	int length = sumString.size();
+	for (int i = 0; i < length; i++) {
+		cout << hashArray[sumString[i] - '0'];
+		if (i != length - 1)
+			cout << " ";
+	}
+	cout << endl;
+	return 0;
+}
